faddc17da2e24df95144bc812fb8a8784230dba3
Mohammad Ziaul Hoque <theprolancerbd@gmail.com>
2022-10-30 02:22:15 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const { Strategy } = require('passport-google-oauth20');
+
+require('dotenv').config();
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
+}
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+    console.log('Google Profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// Read the session form the cookie
+passport.deserializeUser((id, done) => {
+    done(null, id);
+});
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function checkLoggedIn(req, res, next){
+    const isLoggedIn = req.isAuthenticated() && req.user;
+
+    if(!isLoggedIn){
+        res.status(401).json({
+            error: "You are not logged in!"
+        });
+    }
+
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+    res.send('Here is your secret!!!');
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: true
+}), 
+(req, res) => {
+    // res.redirect();
+    console.log('Google called us back!');
+});
+
+app.get('/auth/logout', (req, res) => {
+    req.logout();
+    return res.redirect('/');
+});
+
+app.get('/failure', (req, res) => {
+    res.send('failed to logged in');
+});
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app)
+.listen(3100, () => {
+    console.log('Listening to the port 3100');
+});
